3b466d194b927a43383f3b25981d7f0bb72461c1
Mith227 <dhanukyuba@gmail.com>
2021-05-15 13:55:44 -0700

    commit on mail

--- register.php
+++ register.php
@@ -1,199 +1,48 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        * {
-            box-sizing: border-box;
-        }
-
-        /* style the container */
-        .container {
-            position: relative;
-            border-radius: 5px;
-            background-color: #f2f2f2;
-            padding: 20px 0 30px 0;
-        }
-
-        /* style inputs and link buttons */
-        input,
-        .btn {
-            width: 100%;
-            padding: 12px;
-            border: none;
-            border-radius: 4px;
-            margin: 5px 0;
-            opacity: 0.85;
-            display: inline-block;
-            font-size: 17px;
-            line-height: 20px;
-            text-decoration: none;
-            /* remove underline from anchors */
-        }
-
-        input:hover,
-        .btn:hover {
-            opacity: 1;
-        }
-
-        /* add appropriate colors to fb, twitter and google buttons */
-        .fb {
-            background-color: #3B5998;
-            color: white;
-        }
-
-        .twitter {
-            background-color: #55ACEE;
-            color: white;
-        }
-
-        .google {
-            background-color: #dd4b39;
-            color: white;
-        }
-
-        /* style the submit button */
-        input[type=submit] {
-            background-color: #04AA6D;
-            color: white;
-            cursor: pointer;
-        }
-
-        input[type=submit]:hover {
-            background-color: #45a049;
-        }
-
-        /* Two-column layout */
-        .col {
-            float: left;
-            width: 50%;
-            margin: auto;
-            padding: 0 50px;
-            margin-top: 6px;
-        }
-
-        /* Clear floats after the columns */
-        .row:after {
-            content: "";
-            display: table;
-            clear: both;
-        }
-
-        /* vertical line */
-        .vl {
-            position: absolute;
-            left: 50%;
-            transform: translate(-50%);
-            border: 2px solid #ddd;
-            height: 175px;
-        }
-
-        /* text inside the vertical line */
-        .vl-innertext {
-            position: absolute;
-            top: 50%;
-            transform: translate(-50%, -50%);
-            background-color: #f1f1f1;
-            border: 1px solid #ccc;
-            border-radius: 50%;
-            padding: 8px 10px;
-        }
-
-        /* hide some text on medium and large screens */
-        .hide-md-lg {
-            display: none;
-        }
-
-        /* bottom container */
-        .bottom-container {
-            text-align: center;
-            background-color: #666;
-            border-radius: 0px 0px 4px 4px;
-        }
-
-        /* Responsive layout - when the screen is less than 650px wide, make the two columns stack on top of each other instead of next to each other */
-        @media screen and (max-width: 650px) {
-            .col {
-                width: 100%;
-                margin-top: 0;
-            }
-
-            /* hide the vertical line */
-            .vl {
-                display: none;
-            }
-
-            /* show the hidden text on small screens */
-            .hide-md-lg {
-                display: block;
-                text-align: center;
-            }
-        }
-    </style>
-    <?php
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <?php
     include("assets/includes/links.php");
     ?>
+    
+    <style>
+      .formBox{
+        box-shadow: 0px 0px 5px grey;
+      }
+      .img{
+        width:100px;
+        text-align:center;
+        margin:0 35%;
+      }
+    </style>
 </head>
-
 <body>
-
-    <h2>Responsive Social Login Form</h2>
-    <p>Resize the browser window to see the responsive effect. When the screen is less than 650px wide, make the two columns stack on top of each other instead of next to each other.</p>
-
-    <div class="container">
-        <form action="/action_page.php">
-            <div class="row">
-                <h2 style="text-align:center">Login with Social Media or Manually</h2>
-                <div class="vl">
-                    <span class="vl-innertext">or</span>
-                </div>
-
-                <!-- <div class="col">
-                    <a href="#" class="fb btn">
-                        <i class="fa fa-facebook fa-fw"></i> Login with Facebook
-                    </a>
-                    <a href="#" class="twitter btn">
-                        <i class="fa fa-twitter fa-fw"></i> Login with Twitter
-                    </a>
-                    <a href="#" class="google btn"><i class="fa fa-google fa-fw">
-                        </i> Login with Google+
-                    </a>
-                </div> -->
-
-                <div class="col">
-                    <div class="hide-md-lg">
-                        <p>Or sign in manually:</p>
-                    </div>
-                    <form action="">
-                        <input type="text" name="name" placeholder="Username" required>
-                        <input type="email" name="email" placeholder="UserEmail" required>
-                        <input type="password" name="password" placeholder="Password" required>
-                        <input type="submit" value="Register">
-                    </form>
-                    
-                </div>
-
-            </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-4 formBox mt-4 p-4 mx-auto">
+      <img src="assets/images/dhanuk.jpg" alt="" class="img">
+        <br>
+        <br>
+        <form action="" method="post">
+          <label for="">Name:</label>
+          <input class="form-control" type="text" name="" id="">
+          <label for="">Email:</label>
+          <input  class="form-control" type="email" name="" id="">
+          <label for="">Password:</label>
+          <input class="form-control" type="password" name="" id="">
+          <label for="">Confirm Password:</label>
+          <input class="form-control" type="password" name="" id="">
+          <br>
+          <input type="submit" value="Register" class='btn'>
         </form>
+        <br>
+        <p>Already registered ? Login <a href="login.php">here</a></p>
+      </div>
     </div>
-
-    <div class="bottom-container">
-        <div class="row">
-            <div class="col">
-                <a href="login.php" style="color:white" class="btn">Sign In</a>
-            </div>
-            <div class="col">
-                <a href="forget-password.php" style="color:white" class="btn">Forgot password?</a>
-            </div>
-        </div>
-    </div>
-
+  </div>
 </body>
-
 </html>
